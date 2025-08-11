8d269119b12f6bf5b8d7c4c9239b063d1104c952
jpt333 <jpt4542@gmail.com>
2025-08-10 19:41:07 -0600

    astro works in angular now!

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AstroComponentsModule, RuxGlobalStatusBar } from '@astrouxds/angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: false,
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('Front-End-Developer-Take-Home-Challenge-Jen');
-}
+export class AppComponent {}
